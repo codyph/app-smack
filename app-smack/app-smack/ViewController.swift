e4dda3d44a59bc62923b3cc231aa3fee3c2a49fe
codyph <codyphilipp@gmail.com>
2019-01-14 19:16:26 +0800

    Initial commit

---
+++ app-smack/app-smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-smack
+//
+//  Created by Bailey Aldridge on 14/1/19.
+//  Copyright © 2019 Cody Philipp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
