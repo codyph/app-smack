86f3269229993d7e833f57bfc472d15c4e065cc9
codyph <codyphilipp@gmail.com>
2019-01-17 19:45:56 +0800

    lesson06

--- app-smack/app-smack/Utilities/Constants.swift
+++ app-smack/app-smack/Utilities/Constants.swift
@@ -8,7 +8,20 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = "https://app-smackchatty.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register/"
+
 // Segues
 let TO_LOGIN = "toLoginVC"
 let TO_CREATE_ACCOUNT = "toCreateAccountVC"
 let UNWIND = "unwindToChannelVC"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+
