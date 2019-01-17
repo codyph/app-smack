2589c07adabe7458e0f11281265c1d7e4b8f30dd
codyph <codyphilipp@gmail.com>
2019-01-17 18:26:17 +0800

    lesson04

---
+++ app-smack/app-smack/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  app-smack
+//
+//  Created by Bailey Aldridge on 17/1/19.
+//  Copyright © 2019 Cody Philipp. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+    
+    @IBAction func closeBtnTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
---
+++ app-smack/app-smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  app-smack
+//
+//  Created by Bailey Aldridge on 17/1/19.
+//  Copyright © 2019 Cody Philipp. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLoginVC"
