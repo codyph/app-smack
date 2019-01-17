2422fd1332bb9dfcdc748b4f2882afd2d714cda0
codyph <codyphilipp@gmail.com>
2019-01-17 20:03:06 +0800

    lesson07

--- app-smack/app-smack/Controller/CreateAccountVC.swift
+++ app-smack/app-smack/Controller/CreateAccountVC.swift
@@ -10,10 +10,37 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
+    }
+    
+    
+    @IBAction func chooseAvatarTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func pickBGColorTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func createAccountBtnTapped(_ sender: Any) {
+        guard let email = emailTxt.text , emailTxt.text != "" else {return}
+        guard let password = passwordTxt.text , passwordTxt.text != "" else {return}
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("Registered user!")
+            }
+        }
     }
     
     @IBAction func closeBtnTapped(_ sender: Any) {
---
+++ app-smack/app-smack/View/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  app-smack
+//
+//  Created by Bailey Aldridge on 17/1/19.
+//  Copyright © 2019 Cody Philipp. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
